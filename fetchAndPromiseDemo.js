2894994b16d2276f005b9c1928bfbe6dd598d22f
kingauther33 <64845909+kingauther33@users.noreply.github.com>
2021-08-04 19:11:20 +0700

    Update Training

---
+++ fetchAndPromiseDemo.js
@@ -0,0 +1,58 @@
+// const firstReq = new XMLHttpRequest();
+
+// firstReq.addEventListener('load', function () {
+// 	console.log('FIRST REQUEST WORKED!!!');
+// 	const data = JSON.parse(this.responseText);
+// 	const filmURL = data.results[0].films[0];
+// 	const filmReq = new XMLHttpRequest();
+// 	filmReq.addEventListener('load', function () {
+// 		console.log('SECOND REQUEST WORKED!!!');
+// 		const filmData = JSON.parse(this.responseText);
+// 		console.log(filmData);
+// 	});
+// 	filmReq.addEventListener('error', function (e) {
+// 		console.log('ERROR!!!', e);
+// 	});
+// 	filmReq.open('GET', filmURL);
+// 	filmReq.send();
+// });
+
+// firstReq.addEventListener('error', (e) => {
+// 	console.log('ERROR!!!', e);
+// });
+
+// firstReq.open('GET', 'https://swapi.dev/api/planets/');
+// firstReq.send();
+
+// console.log('Request Sent!');
+
+const checkStatusAndParse = (response) => {
+	if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+	return response.json();
+};
+
+const printPlanets = (data) => {
+	console.log('Fetch ALL PLANETS (FIRST 10)');
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	return Promise.resolve(data.next);
+};
+
+const fetchNextPlanet = (url = 'https://swapi.dev/api/planets/') => {
+	return fetch(url);
+};
+
+fetchNextPlanet()
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanet)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanet)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.catch((err) => {
+		console.log('SOMETHING WENT WRONG WITH FETCH!');
+		console.log(err);
+	});
